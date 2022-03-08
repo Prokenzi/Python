a28118422842962b80cccb0fa6b85f094b0935dc
Prokenzi <prokenzi@gmail.com>
2022-03-08 09:16:06 +0100

    Update kod-10_zdravo.py

--- kod-10_zdravo.py
+++ kod-10_zdravo.py
@@ -1,7 +1,9 @@
 print("Zdravo svete!")
+
 karakter1 = "Picolo"
 karakter2 = "Krilin"
 godine = "22"
+
 
 print("U dalekoj proslosti ziveo je " + karakter1)
 print(karakter1 + " je imao " + godine + " godina")
