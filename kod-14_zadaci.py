3047d29d5c4c37f6a496088ff631e7487e4a4092
milan-prokic <milan.prokic@hpe.com>
2023-05-24 11:02:20 +0200

    b

---
+++ kod-14_zadaci.py
@@ -0,0 +1,12 @@
+print("Milan Prokic")
+
+print("____________")
+print("|           | ")
+print("|           |")
+print("|___________|")
+
+
+print("       /\\")
+print("      /  \\ ")
+print("     /    \\ ")
+print("    /______\\")
