6c6cc0ca62ae939deea17a74ac6e2514af976605
milan-prokic <milan.prokic@hpe.com>
2023-06-02 17:15:50 +0200

    funkcije sa stringovima

--- kod-166_rad-sa-stringovima.py
+++ kod-166_rad-sa-stringovima.py
@@ -1,5 +1,33 @@
-sajt_naziv = "programiranje "
+sajt_naziv = "programiranje"
 #             0123456789
 sajt_slogan = "besplatni online kurs "
 
+# print(sajt_naziv[4]) 
+# print(sajt_naziv[4:9])
+# print(sajt_naziv[0:10])
+# print(sajt_naziv[-4])
 
+# print(sajt_naziv.index('a'))
+# print(sajt_naziv.index('ranje'))
+
+#print(sajt_naziv.index('anja')) #greska
+# print(sajt_naziv.find('anja')) #-1
+
+# print(sajt_naziv.replace("program","tatatatat"))
+godine = "2023"
+
+# print(sajt_naziv.isalpha())
+# print(godine.isdigit())
+#formatiranje
+# format()
+
+#print("{0} {1}".format(sajt_naziv,sajt_slogan))
+#print(f"{sajt_naziv} : {sajt_slogan}")
+
+# ord() i chr() prevodi u ASSCII
+
+print(ord('A'))
+print(ord('a'))
+
+print(chr(98))
+print(chr(66))
