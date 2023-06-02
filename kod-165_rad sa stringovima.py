9dff36daeb4ec6284cd733ef8e0cc416e2e499d3
milan-prokic <milan.prokic@hpe.com>
2023-06-02 15:52:09 +0200

    add string work

---
+++ kod-165_rad sa stringovima.py
@@ -0,0 +1,5 @@
+print("rad sa stringovima \n online kurs")
+sajt_naziv = "programiranje"
+sajt_slogan = "besplatni kurs"
+print(sajt_naziv + " " + sajt_slogan)
+
---
+++ kod-166_rad-sa-stringovima.py
@@ -0,0 +1,5 @@
+sajt_naziv = "programiranje "
+#             0123456789
+sajt_slogan = "besplatni online kurs "
+
+
