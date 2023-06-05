3ffa50df00657a6d2caaa8b564725b2ca308885d
milan-prokic <milan.prokic@hpe.com>
2023-06-05 15:11:55 +0200

    funkcije brojevi

---
+++ kod-167_brojevi.py
@@ -0,0 +1,14 @@
+#int() - integer
+#float() - decimalni
+# print(type("3"))
+# print(10)
+# print(type(3.14))
+# #+.-./.*.%
+
+# print(3+4)
+# print(9/3)
+# print(9%4)
+# print(3*3.1
+moj_broj1 = 13
+moj_broj2 = 17
+print("Zbir dva broja je : " + str(moj_broj1 + moj_broj2))
---
+++ kod168_finkcije_nad_brojevima.py
@@ -0,0 +1,16 @@
+# math()
+# broj = -2
+# print(broj)
+# print(abs(broj))
+
+# print(pow(4,2))
+# print(pow(3,3))
+# print(max(5,8))
+# print(min(4,-3))
+# print(round(3.3333))
+# print(round(4.6))
+from math import *
+
+print(floor(3.6))
+print(ceil(3.6))
+print(sqrt(9))
