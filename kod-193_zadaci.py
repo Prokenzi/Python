53baa9639839f348ee4578f5670af882c3ab89a9
milan-prokic <milan.prokic@hpe.com>
2023-06-06 11:13:20 +0200

    reseni zadaci

---
+++ kod-193_zadaci.py
@@ -0,0 +1,27 @@
+#Prvi zadatak
+# ime = input("UNesite vase ime: ")
+# print((ime + " ") * 5 )
+
+
+#Drugi zadatak
+# prva_rec = input("Unesite prvu rec: ")
+# druga_rec = input("Unesite drugu rec: ")
+
+# print(f"{prva_rec}    {druga_rec}")
+
+#Treci zadatak
+# prvi_broj = int(input("Unesite prvi broj: "))
+# drugi_broj = int(input("Unesite drugi broj: "))
+# treci_broj = int(input("Unesite treci broj: "))
+# aritmeticka = (prvi_broj + drugi_broj + treci_broj) / 3
+# print(f"Aritmeticka sredina brojeva je:  {aritmeticka} ")
+
+#Cetvrti zadatak
+# ord chr()
+# karakter_unos = input("UNesi karakter: ")
+# ascii_karakter = ord(karakter_unos)
+# print(f"Vrednost karaktera u ascii tabeli je: {ascii_karakter}")
+
+#Peti zadatak
+print(f"Omiljena pesma od 'Toze' \
+- \" Trazicu ljubav novu \" !")
