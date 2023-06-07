99fa02f54637c8023d87ed0ade979f2c1b070957
milan-prokic <milan.prokic@hpe.com>
2023-06-07 14:26:45 +0200

    tuple

---
+++ kod_220_rad_sa_torkama.py
@@ -0,0 +1,31 @@
+karakteri = ('Milan', 'Proka','Prokic', 21, 5.55 )
+# print(karakteri)
+# print(type(karakteri))
+
+# karakteri = ('Proka',)
+# print(karakteri)
+# print(type(karakteri))
+
+# koordinate = [(4,5),(5,6),(3,7)]
+# print(koordinate)
+# print(type(koordinate))
+
+# koordinate =(4,5)
+# print(koordinate)
+# print(koordinate[0])
+# print(koordinate[1])
+# koordinate[0] = 8
+# print(len(karakteri))
+
+# print(karakteri[-1])
+# karakter_naziv = "Proka"
+# karakter_naziv = tuple(karakter_naziv)
+# print(karakter_naziv)
+# broj = 13
+# broj = tuple(str(broj))
+# print(broj)
+
+# karakter = karakter_naziv + broj
+# print(karakter)
+del(karakteri)
+print(karakteri)
