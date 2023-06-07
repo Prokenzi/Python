70062f060b615334d124b353e2f38213b883f585
milan-prokic <milan.prokic@hpe.com>
2023-06-07 12:14:21 +0200

    listei funkcije

---
+++ kod-191-rad-sa-listama.py
@@ -0,0 +1,14 @@
+# Rad sa listama
+# list[]
+
+prazna_lista = []
+print(type(prazna_lista))
+karakteri = ["Milan", "Prokic","Resnik", "Dubokic", "Kragujevac", "Sumadija"]
+#print(karakteri)
+
+# print(type(karakteri[4]))
+# print(karakteri[0])
+# print(karakteri[-1])
+# print(karakteri[1:4])
+karakteri[4] = "Dublin"
+print(karakteri)
---
+++ kod-211_funkcije_nad_listama.py
@@ -0,0 +1,43 @@
+karakteri = ["Milan", "Prokic", "Resnik", "Dubokic", "Kragujevac", "Sumadija", "Resnik"]
+
+loto_brojevi = [1,4,7,3,18,22,1]
+
+# print(karakteri)
+# print(loto_brojevi)
+
+# karakteri.extend(loto_brojevi)
+# print(karakteri)
+# karakteri.append("Zacin")
+# print(karakteri)
+# karakteri.insert(2,"Proka")
+
+# # karakteri.remove("Sumadija")
+
+# # del(karakteri[2])
+# # print(karakteri)
+# izbrisani_element = karakteri.pop(2)
+# print(izbrisani_element)
+# karakteri.clear()
+print(karakteri)
+# del() - funkcija
+# .clear() - metoda
+
+# print(len(karakteri))
+# print(karakteri.index("Prokic"))
+# print(karakteri.count("Resnik"))
+# print(loto_brojevi.count(1))
+# karakteri.sort()
+# print(karakteri)
+# print(loto_brojevi)
+# loto_brojevi.sort()
+# print(loto_brojevi)
+
+print(karakteri)
+# karakteri.reverse()
+# print(karakteri)
+# novi_karakteri = karakteri.copy()
+# print(novi_karakteri)
+ime_karaktera = "Proka"
+print(list(ime_karaktera))
+print(min(karakteri))
+print(max(loto_brojevi))
