296baa719aad2fbf78729394857bc51b8afa3243
milan-prokic <milan.prokic@hpe.com>
2023-06-15 16:07:56 +0200

    funkcije

---
+++ kod-255_ra_sa_funkcijama.py
@@ -0,0 +1,17 @@
+# def naziv_funkcije(parametri):
+
+
+# def pozdrav():
+#     print("Zdravo")
+
+   
+# pozdrav()
+# pozdrav()
+karakter_ime = input("Unesite ime : ")
+karakter_godine = input("Unesite godine : ")
+
+def pozdrav(ime,godine):
+    print("Zdravo " + ime)
+    print(ime + " ima " + godine)
+
+pozdrav(karakter_ime,karakter_godine)
